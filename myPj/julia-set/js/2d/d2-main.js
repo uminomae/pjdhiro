832503267cd5e763b564ea26fee88b767266f30c
hioikawa <uminomae.com>
2025-06-03 10:39:03 +0900

    html 分割:navbar

--- myPj/julia-set/js/2d/d2-main.js
+++ myPj/julia-set/js/2d/d2-main.js
@@ -1,7 +1,14 @@
-// ファイル: js/2d/d2-main.js
-
+// ファイル: js/2d/d2-main.js (修正版)
 import { initApp } from './d2-init-app.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+/** 
+ * DOM がすでに完全に読み込まれている (interactive or complete) なら即座に initApp()
+ * そうでなければ DOMContentLoaded を待つ
+ */
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+  });
+} else {
   initApp();
-});
+}
